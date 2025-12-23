64433670f0e65c953dbcbf51815cf61edab5b39d
nherimam <herimaminavalonaprosper@gmail.com>
2025-12-24 00:20:06 +0300

    _count_harvest

---
+++ Python_Module_00/ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/ python3
+
+def cout(number, value):
+    print(value)
+    if (number > 0):
+        value += 1
+        number -= 1
+        cout(number, value)
+
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: ")) - 1
+    out = 1
+    cout(days, out)
+    print("Harvest time!")
