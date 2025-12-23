5e51621613c657a7e8b7742764f0cc2514a08af3
nherimam <herimaminavalonaprosper@gmail.com>
2025-12-24 00:24:53 +0300

    ft_garden_summary

---
+++ Python_Module_00/ex6/ft_garden_summary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env/ python3
+
+def ft_garden_summary():
+    comm_garden = input("Enter garden name: ")
+    plants = input("Enter number of plants: ")
+    print("Garden: " + comm_garden)
+    print("Plants: " + plants)
+    print("Status: Growing well!")
