ded843e9e73ba0035836f67fee3c67024c8cdee4
nherimam <herimaminavalonaprosper@gmail.com>
2026-01-27 09:21:51 +0300

    ft_score_analytics || ft_command_quest  # Changes to be committed: 	new file:   PythonModule03/data_quest_tools/advanced_data_helper.py 	new file:   PythonModule03/data_quest_tools/data_generator.py 	new file:   PythonModule03/data_quest_tools/exercise_0_help.py 	new file:   PythonModule03/data_quest_tools/exercise_1_helper.py 	new file:   PythonModule03/ex0/ft_command_quest.py 	new file:   PythonModule03/ex0/ft_score_analytics.py

---
+++ PythonModule03/ex0/ft_command_quest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+import sys
+
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    if len(sys.argv) < 2:
+        print("No arguments provided!")
+    print(f"Program name: {sys.argv[0]}")
+    if len(sys.argv) > 1:
+        print(f"Arguments received: {len(sys.argv) - 1}")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"Argument {i}: {arg}")
+    print(f"Total arguments: {len(sys.argv)}")
