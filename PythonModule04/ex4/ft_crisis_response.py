99a4f0831e086d8ed70105c92885a81b85feee71
nherimam <herimaminavalonaprosper@gmail.com>
2026-01-29 11:27:19 +0300

    ft_crisis_response  # Changes to be committed: 	new file:   PythonModule04/ex4/ft_crisis_response.py

---
+++ PythonModule04/ex4/ft_crisis_response.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+
+import sys
+
+
+class SVault:
+    def __init__(self, path):
+        self.path = path
+
+    def load(self):
+        try:
+            with open(self.path, 'r') as f:
+                content = f.read()
+            print("SUCCESS: Archive recovered - ''", content, "''", sep='')
+            print("STATUS: Normal operations resumed")
+        except FileNotFoundError:
+            print("RESPONSE: Archive not found in storage matrix")
+            print("STATUS: Crisis handled, system stable")
+        except PermissionError:
+            print("RESPONSE: Security protocols deny access")
+            print("STATUS: Crisis handled, security maintained")
+
+
+def main() -> None:
+    print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===")
+    print()
+
+    print("CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
+    crisis1 = SVault("lost_archive.txt")
+    crisis1.load()
+    print()
+
+    path = "../data-generator-tools/security_protocols.txt"
+    print(f"CRISIS ALERT: Attempting access to '{path}'...")
+    crisis1 = SVault(path)
+    crisis1.load()
+    print()
+
+    path = "../data-generator-tools/standard_archive.txt"
+    print(f"CRISIS ALERT: Attempting access to '{path}'...")
+    crisis1 = SVault(path)
+    crisis1.load()
+    print()
+
+    print("All crisis scenarios handled successfully. Archives secure.")
+
+
+if __name__ == '__main__':
+    main()
