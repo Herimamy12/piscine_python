f444a8b951e8392c64cf53e2dcebda250d234f61
Herimamy12 <herimaminavalonaprosper@gmail.com>
2026-05-10 18:41:50 +0300

    Hello.py Changes to be committed: 	new file:   ForDataScience/Python_0_Starting/ex00/Hello.py

---
+++ ForDataScience/Python_0_Starting/ex00/Hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Madagascar!")
+ft_set = {"Hello", "Antananarivo!"}
+ft_dict = {"Hello" : "42 Antananarivo!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
